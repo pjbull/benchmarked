e256babeb33311aa01fee59b730274567fedba3f
Peter Bull <pjbull@gmail.com>
2013-12-11 13:50:15 -0500

    Update Executed Process Notebook

--- data/get_sc_data.sh
+++ data/get_sc_data.sh
@@ -1,5 +1,5 @@
 
-for (( year = 1946; year > 1945; year-- )); do
+for (( year = 2012; year > 1945; year-- )); do
 	file=scotus$year.xml.gz
 	echo $file
 	curl -L -o $file https://www.courtlistener.com/api/bulk/$year/scotus.xml.gz
